d78c37f7688a94677460e464217b690686d52b84
Trepachko Ann <trepachko.ann@gmail.com>
2020-08-23 18:18:15 +0300

    выполнено 4е задание

--- src/array-practice/task4.js
+++ src/array-practice/task4.js
@@ -7,3 +7,9 @@
  * console.log(union([5, 1, 2, 3, 3], [4, 3, 2])); -> [5, 1, 2, 3, 4]
  * console.log(union([5, 1, 3, 3, 4], [1, 3, 4])); -> [5, 1, 3, 4]
  */
+
+function union(arr1, arr2) {
+  let set = new Set([ ...arr1, ...arr2 ]);
+
+  return Array.from(set);
+}
