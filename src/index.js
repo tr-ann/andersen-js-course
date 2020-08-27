82e3e728fea78af67444b5bd4b3c6cb243a44fa8
Trepachko Ann <trepachko.ann@gmail.com>
2020-08-27 18:11:53 +0300

    выполнено 6ое задание es-features

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import { task2Old, task2New } from './es-features/task2';
 import { task3Old, task3New } from './es-features/task3';
 import { task4Old, task4New } from './es-features/task4';
 import { task5Old, task5New } from './es-features/task5';
-// import { task6Old, task6New } from './es-features/task6';
+import { task6Old, task6New } from './es-features/task6';
 import { task7Old, task7New } from './es-features/task7';
 import { task8Old, task8New } from './es-features/task8';
 import { task9Old, task9New } from './es-features/task9';
@@ -53,8 +53,8 @@ console.log('task5 new', task5New(['test', 42], { name: 'foo', val: 7 })); // ['
 
 console.log('=============');
 
-// console.log('task6 old', task6Old()); // ['Max', 100, 'Admin', false, '1']
-// console.log('task6 new', task6New()); // ['Max', 100, 'Admin', false, '1']
+console.log('task6 old', task6Old()); // ['Max', 100, 'Admin', false, '1']
+console.log('task6 new', task6New()); // ['Max', 100, 'Admin', false, '1']
 
 console.log('=============');
 
